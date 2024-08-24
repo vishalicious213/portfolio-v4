2a1a16687962f273345f1ca3e0e427cb4ec7f054
vishalicious213 <vishalicious213@gmail.com>
2024-08-24 16:11:17 -0700

    removed buttons from main. added 2nd button in #my-work so we have one to show websites and another to show applications (easier to understand for users than toggle button). added event listeners to toggle between showing #websites and #applications based on button click

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-const toggleBtn = document.getElementById("work-btn")
+const appBtn = document.getElementById("app-btn")
+const webBtn = document.getElementById("web-btn")
 const apps = document.getElementById("applications")
 const sites = document.getElementById("websites")
 
-toggleBtn.addEventListener("click", () => {
-    let btnText = toggleBtn.innerText
-    
-    if (btnText === "SEE APPLICATIONS") {
-        toggleBtn.innerText = "SEE WEBSITES"
-        apps.classList.remove("hidden")
-        sites.classList.add("hidden")
-    } else if (btnText === "SEE WEBSITES") {
-        toggleBtn.innerText = "SEE APPLICATIONS"
-        sites.classList.remove("hidden")
-        apps.classList.add("hidden")
-    }
+appBtn.addEventListener("click", () => {    
+    apps.classList.remove("hidden")
+    sites.classList.add("hidden")
+})
+
+webBtn.addEventListener("click", () => {    
+    sites.classList.remove("hidden")
+    apps.classList.add("hidden")
 })
