173d82fd75e7ccb1ed7ce5995e19882dce84ea9d
vishalicious213 <vishalicious213@gmail.com>
2024-08-24 16:51:00 -0700

    updated #websites and #applications with .gallery class in event listeners

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const sites = document.getElementById("websites")
 
 appBtn.addEventListener("click", () => {    
     apps.classList.remove("hidden")
+    apps.classList.add("gallery")
     sites.classList.add("hidden")
+    sites.classList.remove("gallery")
 })
 
 webBtn.addEventListener("click", () => {    
     sites.classList.remove("hidden")
+    sites.classList.add("gallery")
     apps.classList.add("hidden")
+    apps.classList.remove("gallery")
 })
