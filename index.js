5773602fac1c549e7966d589cd13d9a307b93d72
vishalicious213 <vishalicious213@gmail.com>
2024-08-24 15:20:00 -0700

    clicking toggleBtn switches between .applications and .websites

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ toggleBtn.addEventListener("click", () => {
     
     if (btnText === "SEE APPLICATIONS") {
         toggleBtn.innerText = "SEE WEBSITES"
+        apps.classList.remove("hidden")
+        sites.classList.add("hidden")
     } else if (btnText === "SEE WEBSITES") {
         toggleBtn.innerText = "SEE APPLICATIONS"
+        sites.classList.remove("hidden")
+        apps.classList.add("hidden")
     }
 })
