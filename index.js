5171e801be938350c5fc9f070db7a4d160a08687
vishalicious213 <vishalicious213@gmail.com>
2024-08-25 11:30:35 -0700

    removed .project from html. added it to renderApps() to test

--- index.js
+++ index.js
@@ -20,6 +20,31 @@ webBtn.addEventListener("click", () => {
 
 function renderApps() {
     apps.innerHTML = `
-        <h1>Updated Applications</h1>
+        <section class="project">
+            <div>
+                <img class="screenshot" src="img/projects/movies.jpg" alt="">
+            </div>
+            <div class="inner-project-container">
+                <h4>Movies Watchlist</h4>
+                <div class="project-buttons">
+                    <div class="visit-site">
+                        <span class="visit-icon">&#x2BC8;</span>
+                        Visit site
+                    </div>
+                    <div class="view-code">
+                        <i class="fab fa-github"></i>
+                        View code
+                    </div>
+                </div>
+                <section class="project-info">
+                    <p class="project-desc">Uses the OMDB (Online Movie Database) API to get movie info from user searches. Users can add movies to or remove movies from a personal watchlist. Includes pagination for more results.</p>
+                    <div class="stack">
+                        <img src="img/html5-color.svg" alt="">
+                        <img src="img/css3-color.svg" alt="">
+                        <img src="img/javascript-color.svg" alt="">
+                    </div>
+                </section>
+            </div>
+        </section>
     `
 }
