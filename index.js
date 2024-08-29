43e628d08fbfaa55b9790f50a0e9045a3f0fbe57
vishalicious213 <vishalicious213@gmail.com>
2024-08-28 23:21:53 -0700

    added js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const appBtn = document.getElementById("app-btn")
 const webBtn = document.getElementById("web-btn")
 const apps = document.getElementById("applications")
 const sites = document.getElementById("websites")
+let slideIndex = 1
 
 const appsArray = [
     {
@@ -155,4 +156,34 @@ function renderSites(arr) {
     })
 }
 
+function plusSlides(n) {
+    showSlides(slideIndex =+ n)
+}
+
+function showSlides(n) {
+    let i
+    let slides = document.getElementsByClassName("slide")
+    let dots = document.getElementsByClassName("dot")
+
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "")
+    }
+
+    slides[slideIndex-1].style.display = "block"
+    dots[slideIndex-1].className += " active"
+}
+
 // renderSites(siteArray)
+showSlides(slideIndex)
