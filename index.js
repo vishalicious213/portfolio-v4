a4a73a09532dc0877cf2631037b82d84ea0c7be0
vishalicious213 <vishalicious213@gmail.com>
2024-09-01 22:42:19 -0700

    added app-btn & web-btn event listeners

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const webBtn = document.getElementById("web-btn")
 
 // ⬇️ EVENT LISTENERS ⬇️
 
+appBtn.addEventListener("click", appClick)
+webBtn.addEventListener("click", webClick)
+
 // ⬇️ EVENT HANDLERS ⬇️
 
 function changeSlide(n) {
