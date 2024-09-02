c026490d1e50ca445c945ef0ad4b7f177334ec9a
vishalicious213 <vishalicious213@gmail.com>
2024-09-01 22:48:24 -0700

    added appsArray, siteArray, dataSet

--- index.js
+++ index.js
@@ -2,6 +2,70 @@ const appBtn = document.getElementById("app-btn")
 const webBtn = document.getElementById("web-btn")
 const carouselImg = document.getElementById("carousel-images")
 
+const appsArray = [
+    {
+        id: 1,
+        img: "img/projects/movies.jpg",
+        title: "Movies Watchlist",
+        view: "https://vish213-movies.netlify.app/",
+        code: "https://github.com/vishalicious213/movies",
+        desc: "Add or remove movies from your personal watchlist. Search for a movie and get information from the OMDB (Online Movie Database) API about it. Save the ones you want to see for later.",
+        stack: ["html", "css", "js"]
+    },
+    {
+        id: 2,
+        img: "img/projects/jimmys-diner.jpg",
+        title: "Jimmy's Diner",
+        view: "https://vish213-jimmys-diner.netlify.app/",
+        code: "https://github.com/vishalicious213/restaurant-ordering-app",
+        desc: "Jimmy's Diner serves the best burgers and pizzas in town. This kiosk/ordering app was prepared using the finest JavaScript and seasoned with arrays, objects and a sprinkling of CSS.",
+        stack: ["html", "css", "js"]
+    },
+    {
+        id: 3,
+        img: "img/projects/locator.jpg",
+        title: "Locator App",
+        view: "https://vish213-locator.netlify.app/",
+        code: "https://github.com/vishalicious213/locator",
+        desc: "I built this Progressive Web App after moving to CA and never knowing what city I was driving through. It shows the city, local map and weather using Open Weather API and the Google Maps API.",
+        stack: ["html", "css", "js"]
+    },
+    {
+        id: 4,
+        img: "img/projects/task-list.jpg",
+        title: "Task List",
+        view: "https://vish213-task-list.netlify.app/",
+        code: "https://github.com/vishalicious213/tasks-firebase",
+        desc: "A combination shopping list, to-do list and notes app that I built for my family of cat girls. It uses the Firebase Realtime Database to store and update data so changes are visible to everyone immediately.",
+        stack: ["html", "css", "js", "fb"]
+    }
+]
+const siteArray = [
+    {
+        id: 1,
+        img: "img/projects/salon.jpg",
+        title: "Ayesha Salon",
+        view: "https://vish213-salon.netlify.app/",
+        desc: "Welcome to Ayesha Salon, where style meets comfort! Our cozy salon is your go-to destination for the latest hair trends, with a team of skilled stylists passionate about crafting the perfect look for you. Explore our website for a glimpse of our work, customer reviews, and easy booking options.",
+    },
+    {
+        id: 2,
+        img: "img/projects/grill.jpg",
+        title: "Vish's Grillicious Grill",
+        view: "https://vish213-grill.netlify.app/",
+        desc: "At Vish's Grillicious Grill, culinary dreams take flight - and crash in a box of Chicken McNuggets! We aspire to serve home-cooked meals that delight the senses, but every parent knows how that ends. Order a meal and we'll bring out what your kids really want, too.",
+    },
+    {
+        id: 3,
+        img: "img/projects/bcc.jpg",
+        title: "BCC Foundation",
+        view: "https://vish213-bcc-shop.netlify.app/",
+        desc: "The Breast Cancer Comfort Foundation aims to offer encouragement, support and laughter to individuals whose lives have been forever changed by curative or preventative breast cancer treatment through the delivery of unique “Comfort Baskets” and “Chemo Comfort Bags”.",
+    }
+]
+
+let dataSet = appsArray
+
 // ⬇️ EVENT LISTENERS ⬇️
 
 appBtn.addEventListener("click", appClick)
